215ef9aa67c5f0d69d12953b7ee68a422bca3d8b
milkiq <17810390+milkiq@users.noreply.github.com>
2024-01-09 21:21:30 +0800

    修复向量模板类的问题

--- wmath/include/vector.h
+++ wmath/include/vector.h
@@ -33,21 +33,21 @@ namespace WMath {
         };
     };
 
-    template<typename T, size_t N, typename D = VecDataDefault<T, N>>
+    template<typename T, size_t N, typename D = VecDataDefault<T, N>, typename current = D>
     class Vec: public D {
         static_assert(N != 0, "N can't be zero");
         static_assert(N <= 4, "N can't over 4");
 
-        Vec<T, N, D> do_add(const Vec<T, N, D>& other) const {
-            Vec<T, N, D> result;
+        current do_add(const Vec<T, N, D, current>& other) const {
+           current result;
             for (size_t i = 0; i < N; i++) {
                 result[i] = data[i] + other[i];
             }
             return result;
         }
 
-        Vec<T, N, D> do_sub(const Vec<T, N, D>& other) const {
-            Vec<T, N, D> result;
+       current do_sub(const Vec<T, N, D, current>& other) const {
+            current result;
             for (size_t i = 0; i < N; i++) {
                 result[i] = data[i] - other[i];
             }
@@ -63,9 +63,11 @@ namespace WMath {
             }
         }
 
-        Vec(const Vec<T, N, D>& other) {
+        template<typename U, size_t M, typename C, typename inCurrent>
+        Vec(const Vec<U, M, C, inCurrent>& other) {
+            static_assert(M >= N, "M must be greater than or equal to N");
             for (size_t i = 0; i < N; i++) {
-                data[i] = other[i];
+                data[i] = static_cast<T>(other[i]);
             }
         }
 
@@ -77,32 +79,32 @@ namespace WMath {
             return data[index];
         }
 
-        Vec<T, N, D> operator+(const Vec<T, N, D>& other) const {
+        current operator+(const Vec<T, N, D, current>& other) const {
             return do_add(other);
         }
 
-        Vec<T, N, D>& operator+=(const Vec<T, N, D>& other) {
-            Vec<T, N, D> result = do_add(other);
+        current& operator+=(const Vec<T, N, D, current>& other) {
+            current result = do_add(other);
             for (size_t i = 0; i < N; i++) {
                 data[i] = result[i];
             }
             return *this;
         }
 
-        Vec<T, N, D> operator-(const Vec<T, N, D>& other) const {
+        current operator-(const Vec<T, N, D, current>& other) const {
             return do_sub(other);
         }
 
-        Vec<T, N, D>& operator-=(const Vec<T, N, D>& other) {
-            Vec<T, N, D> result = do_sub(other);
+        current& operator-=(const Vec<T, N, D, current>& other) {
+            current result = do_sub(other);
             for (size_t i = 0; i < N; i++) {
                 data[i] = result[i];
             }
             return *this;
         }
 
-        template<typename U, size_t M, typename C>
-        Vec<T, N, D>& operator=(const Vec<U, M, C>& other) {
+        template<typename U, size_t M, typename C, typename inCurrent>
+        current& operator=(const Vec<U, M, C, inCurrent>& other) {
             static_assert(M >= N, "M must be greater than or equal to N");
 
             for (size_t i = 0; i < N; i++) {
@@ -116,7 +118,7 @@ namespace WMath {
             printf("a");
         }
 
-        bool operator==(const Vec<T, N, D>& other) const {
+        bool operator==(const Vec<T, N, D, current>& other) const {
             for (size_t i = 0; i < N; i++) {
                 if (data[i] != other[i]) {
                     return false;
@@ -125,7 +127,7 @@ namespace WMath {
             return true;
         }
 
-        T Dot(const Vec<T, N, D>& other) const {
+        T Dot(const Vec<T, N, D, current>& other) const {
             T result = 0;
             for (size_t i = 0; i < N; i++) {
                 result += data[i] * other[i];
@@ -133,21 +135,21 @@ namespace WMath {
             return result;
         }
 
-        Vec<T, N, D> Normalized() const {
+        current Normalized() const {
             T length = 0;
             for (size_t i = 0; i < N; i++) {
                 length += data[i] * data[i];
             }
             length = sqrt(length);
-            Vec<T, N, D> result;
+            current result;
             for (size_t i = 0; i < N; i++) {
                 result[i] = data[i] / length;
             }
             return result;
         }
 
-        Vec<T, N, D> Cross(const Vec<T, N, D>& other) const {
-            Vec<T, N, D> result;
+        current Cross(const Vec<T, N, D, current>& other) const {
+            current result;
             for (size_t i = 0; i < N; i++) {
                 result[i] = data[(i + 1) % N] * other[(i + 2) % N] - data[(i + 2) % N] * other[(i + 1) % N];
             }
@@ -156,38 +158,64 @@ namespace WMath {
     };
 
     template<typename T>
-    class Vector3: public Vec<T, 3, VecData3<T>> {
+    class Vector4: public Vec<T, 4, VecData4<T>, Vector4<T>> {
+        public:
+            using D = VecData4<T>;
+            using D::x;
+            using D::y;
+            using D::z;
+            using D::w;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::Vec;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator+;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator-;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator=;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator+=;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator-=;
+            using Vec<T, 4, VecData4<T>, Vector4<T>>::operator==;
+            Vector4(T _x, T _y, T _z, T _w);
+    };
+
+    template<typename T>
+    class Vector3: public Vec<T, 3, VecData3<T>, Vector3<T>> {
         public:
             using D = VecData3<T>;
             using D::x;
             using D::y;
             using D::z;
-            using Vec<T, 3, VecData3<T>>::Vec;
-            using Vec<T, 3, VecData3<T>>::operator+;
-            using Vec<T, 3, VecData3<T>>::operator-;
-            using Vec<T, 3, VecData3<T>>::operator=;
-            using Vec<T, 3, VecData3<T>>::operator+=;
-            using Vec<T, 3, VecData3<T>>::operator-=;
-            using Vec<T, 3, VecData3<T>>::operator==;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::Vec;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator+;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator-;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator=;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator+=;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator-=;
+            using Vec<T, 3, VecData3<T>, Vector3<T>>::operator==;
             // Vector3();
             Vector3(T _x, T _y, T _z);
     };
 
     template<typename T>
-    class Vector2: public Vec<T, 2, VecData2<T>> {
+    class Vector2: public Vec<T, 2, VecData2<T>, Vector2<T>> {
         public:
             using D = VecData2<T>;
             using D::x;
             using D::y;
-            using Vec<T, 2, VecData2<T>>::Vec;
-            using Vec<T, 2, VecData2<T>>::operator+;
-            using Vec<T, 2, VecData2<T>>::operator-;
-            using Vec<T, 2, VecData2<T>>::operator=;
-            using Vec<T, 2, VecData2<T>>::operator+=;
-            using Vec<T, 2, VecData2<T>>::operator-=;
-            using Vec<T, 2, VecData2<T>>::operator==;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::Vec;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator+;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator-;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator=;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator+=;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator-=;
+            using Vec<T, 2, VecData2<T>, Vector2<T>>::operator==;
             Vector2(T _x, T _y);
     };
+
+    template<typename T>
+    Vector4<T>::Vector4(T _x, T _y, T _z, T _w) {
+        x = _x;
+        y = _y;
+        z = _z;
+        w = _w;
+    }
 
     template<typename T>
     Vector3<T>::Vector3(T _x, T _y, T _z) {
@@ -202,11 +230,15 @@ namespace WMath {
         y = _y;
     }
 
+    using Vector4i = WMath::Vector4<int>;
+    using Vector4f = WMath::Vector4<float>;
+
     using Vector3i = WMath::Vector3<int>;
     using Vector3f = WMath::Vector3<float>;
 
     using Vector2i = WMath::Vector2<int>;
     using Vector2f = WMath::Vector2<float>;
+
 }
 
 #endif
